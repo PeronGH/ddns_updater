e4e2bfeb16204d0b2cc80cb95e37dbf196e86527
PeronGH <peron_ap@icloud.com>
2025-05-03 00:05:32 +0100

    feat: remove http handler

--- cron.ts
+++ cron.ts
@@ -1,21 +1,10 @@
 import { main } from "./main.ts";
 
-let lastRun = new Date(0);
-let lastRunSuccess = false;
-
-Deno.cron("update ddns", { hour: { every: 1 } }, async () => {
+await Deno.cron("update ddns", { hour: { every: 1 } }, async () => {
   try {
     await main();
-    lastRunSuccess = true;
     console.log("Cron job completed successfully");
   } catch (error) {
-    lastRunSuccess = false;
     console.error("Error running cron job:", error);
-  } finally {
-    lastRun = new Date();
   }
 });
-
-Deno.serve(() =>
-  new Response(`last run: ${lastRun}\nwas successful: ${lastRunSuccess}\n`)
-);
