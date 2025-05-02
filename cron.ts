892177cae1bcb2c88eb03af64d637d2509badd64
PeronGH <peron_ap@icloud.com>
2025-05-02 23:47:30 +0100

    feat: show whether last run was successful

--- cron.ts
+++ cron.ts
@@ -1,16 +1,21 @@
 import { main } from "./main.ts";
 
 let lastRun = new Date(0);
+let lastRunSuccess = false;
 
 Deno.cron("update ddns", { hour: { every: 1 } }, async () => {
   try {
     await main();
+    lastRunSuccess = true;
     console.log("Cron job completed successfully");
   } catch (error) {
+    lastRunSuccess = false;
     console.error("Error running cron job:", error);
   } finally {
     lastRun = new Date();
   }
 });
 
-Deno.serve(() => new Response(`last run: ${lastRun}`));
+Deno.serve(() =>
+  new Response(`last run: ${lastRun}\nwas successful: ${lastRunSuccess}\n`)
+);
