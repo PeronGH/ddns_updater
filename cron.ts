448bec416852e2dc8aec230d4b3ce93f2a1fc934
PeronGH <peron_ap@icloud.com>
2025-05-02 23:45:36 +0100

    feat: show last run

--- cron.ts
+++ cron.ts
@@ -1,3 +1,16 @@
 import { main } from "./main.ts";
 
-await Deno.cron("update ddns", { hour: { every: 1 } }, main);
+let lastRun = new Date(0);
+
+Deno.cron("update ddns", { hour: { every: 1 } }, async () => {
+  try {
+    await main();
+    console.log("Cron job completed successfully");
+  } catch (error) {
+    console.error("Error running cron job:", error);
+  } finally {
+    lastRun = new Date();
+  }
+});
+
+Deno.serve(() => new Response(`last run: ${lastRun}`));
